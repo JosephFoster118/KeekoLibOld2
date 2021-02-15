a6af3181ac8f1efd0c5c9338485cbd6a3c386938
Joseph <Joseph.Foster118@gmail.com>
2021-02-14 20:12:11 -0600

    Converted to GLFW

--- OpenGLFun/src/MultisampledFrameBuffer.cpp
+++ OpenGLFun/src/MultisampledFrameBuffer.cpp
@@ -4,12 +4,14 @@
 MultisampledFrameBuffer::MultisampledFrameBuffer(uint32_t width, uint32_t height, uint16_t samples)
 {
     glEnable( GL_MULTISAMPLE );
+
     this->width = width;
     this->height = height;
     this->samples = samples;
     glGenFramebuffers(1, &fbo);
     glBindFramebuffer(GL_FRAMEBUFFER, fbo);
     glGenTextures(1, &tex);
+
     glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, tex);
     glTexImage2DMultisample( GL_TEXTURE_2D_MULTISAMPLE, samples, GL_RGBA8, width, height, false );
 
@@ -17,11 +19,6 @@ MultisampledFrameBuffer::MultisampledFrameBuffer(uint32_t width, uint32_t height
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); 
     glBindFramebuffer( GL_FRAMEBUFFER, fbo );
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, tex, 0);
-
-    // glGenRenderbuffers(1, &rbo);
-    // glBindRenderbuffer(GL_RENDERBUFFER, rbo);  
-    // glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
-    // glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
     
     proxy_frame_buffer = std::make_unique<FrameBuffer>(width,height);
 }
--- OpenGLFun/src/TestClass.cpp
+++ OpenGLFun/src/TestClass.cpp
@@ -13,7 +13,6 @@ TestClass::TestClass()
 void TestClass::display()
 {
     std::lock_guard<std::mutex> lck (mtx);
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
     //std::lock_guard<std::mutex> lck (mtx);
     glClearColor(0.2f, 0.2f, 0.2f, 1.0f); // Set background color to black and opaque
@@ -22,37 +21,30 @@ void TestClass::display()
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glMatrixMode(GL_PROJECTION);      // Select the Projection matrix for operation
     glLoadIdentity();                 // Reset Projection matrix
-    glViewport(0,0,1000,1000);
-    gluOrtho2D(0.0, 1000.0, 1000.0, 0.0); // Set clipping area's left, right, bottom, top
-    rect3.draw();
+    glViewport(0,0,500,500);
+    gluOrtho2D(0.0, 500.0, 500.0, 0.0); // Set clipping area's left, right, bottom, top
 
     glEnable(GL_TEXTURE_2D);
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
     glBindTexture(GL_TEXTURE_2D, fb->getTexture()); 
-
+    glPushMatrix();
     glBegin(GL_QUADS);
     {
         glTexCoord2f(0, 0);
         glVertex2f(100, 100);
 
         glTexCoord2f(1, 0);
-        glVertex2f(600, 100);
+        glVertex2f(300, 100);
 
         glTexCoord2f(1, 1);
-        glVertex2f(600, 600);
+        glVertex2f(300, 300);
 
         glTexCoord2f(0, 1);
-        glVertex2f(100, 600);
+        glVertex2f(100, 300);
     }
+    glPopMatrix();
     glEnd();
-
-
-
-
     glFlush();  // Render now
-    glutSwapBuffers();
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
 }
 
 void TestClass::generateTexture()
@@ -61,7 +53,7 @@ void TestClass::generateTexture()
     glBindFramebuffer(GL_FRAMEBUFFER, *fb);
 
     glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);      
+    glClear(GL_COLOR_BUFFER_BIT);
 
        
     glEnable(GL_BLEND);
@@ -74,42 +66,11 @@ void TestClass::generateTexture()
     Rectangle rect{4.0f,4.0f,Point{6.5f,6.5f},Color{1.0f,1.0f,1.0f,1.0f}};
     rect.draw();
     glFlush();
-            if(glGetError())
+    if(glGetError())
     {
         printf("Got error!\n");
     }
     fb->blitBuffers();
-
-    
- 
-    // glGenFramebuffers(1, &fbo);
-    // glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-    // glGenTextures(1, &tex);
-    // glBindTexture(GL_TEXTURE_2D, tex);
-    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 10, 10, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); 
-
-    // glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex, 0);
-
-    // glGenRenderbuffers(1, &rbo);
-    // glBindRenderbuffer(GL_RENDERBUFFER, rbo);  
-    // glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 10, 10);
-    // glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
-    
-    
-    
-    // glBindFramebuffer(GL_READ_FRAMEBUFFER, *fb);
-    
-
-    
-    // glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo);
-    
-    
-    // glBlitNamedFramebuffer(*fb, fbo, 0, 0, 10, 10, 0, 0, 10, 10, GL_COLOR_BUFFER_BIT, GL_NEAREST);
-
-
 
 
     struct color
